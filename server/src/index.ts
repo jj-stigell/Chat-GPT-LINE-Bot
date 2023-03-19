e79e3e776062be8c9a16025f94e34e5211f2ead6
Jere Stigell <jj.stigell@gmail.com>
2023-03-19 23:40:29 +0900

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 // Project imports
 import { app } from './app';
 import { PORT } from './configs/environment';
-import { connectToDatabase } from './database/index';
+// import { connectToDatabase } from './database/index';
 
 app.listen(PORT, async () => {
-  await connectToDatabase();
+  // await connectToDatabase();
   console.log(`Application started on PORT: ${PORT} 🎉`);
 });
